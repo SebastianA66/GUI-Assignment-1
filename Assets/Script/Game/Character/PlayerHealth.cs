d2d3ac289f8cde572e0b040c7b366b3f0417e999
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2018-12-06 00:23:05 +1100

    Initial commit

---
+++ Assets/Script/Game/Character/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;                            // The amount of health the player starts the game with.
+    public int currentHealth;                                   // The current health the player has.
+    public Slider healthSlider;                                 // Reference to the UI's health bar.
+    public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
+    public AudioClip deathClip;                                 // The audio clip to play when the player dies.
+    public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
+    public AudioSource audioSource;
+    public UIShake uiShake;
+    public Text timeText;
+
+    public GameObject gameOver;
+                                            
+    
+    CharacterController playerMovement;                         
+                             
+    
+    
+   
+    
+}
